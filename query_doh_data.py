e5902edcfb27f653f43d4d07fd2df51a87fd352b
Ronnie Villanueva <ronnie.code@outlook.com>
2020-04-01 18:11:44 +0600

    pep8

--- query_doh_data.py
+++ query_doh_data.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-
-import requests
+"""Fetches JSON data from https://services5.arcgis.com/mnYJ21GiFTR97WFg and saves it as an .xlsx excel file"""
 import json
+import requests
 import pandas as pd
 
 def main():
+    '''Main function'''
     url = "https://bit.ly/ph_doh_covid19_json"
     response = requests.get(url)
     data = json.loads(response.text)
-    x = json.dumps([y["attributes"] for y in data["features"]])
-    df = pd.DataFrame(pd.read_json(x))
+    data = json.dumps([y["attributes"] for y in data["features"]])
+    df = pd.DataFrame(pd.read_json(data))
     df.to_excel(f"covid19_ph1-ph{len(df)}.xlsx")
 
 if __name__ == '__main__':
     main()
+    
